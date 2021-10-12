4af6bc674c60db3bf2403d8ad349635473736113
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-12 11:35:26 -0400

    Got delete route working

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Employee = require('../models/employees');
 
-router.get('/employee/delete', isAuthenticated, async (req, res) => {
-    await User.deleteMany({});
-    res.redirect('/');
+router.get('/employee/delete/:id', isAuthenticated, (req, res) => {
+    Employee.deleteOne({id: req.params.id},(err, result) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.redirect('/viewEmployees')
+      }
+    });
 });
 
 router.get('/employeeData', isAuthenticated, (req, res) => {
@@ -13,9 +18,13 @@ router.get('/employeeData', isAuthenticated, (req, res) => {
 
 router.get('/viewEmployees', isAuthenticated, (req, res) => {
   Employee.find({},(err,foundEmployees) => {
-    res.render('addEmployee.ejs', {employees: Employee, user: req.session.user})
+    res.render('viewEmployees.ejs', {employees: foundEmployees, user: req.session.user})
   })
 });
+
+router.get('/addEmployee', isAuthenticated, (req,res) => {
+  res.render('/addEmployee.ejs')
+})
 
 router.post('/addEmployee', isAuthenticated, (req, res) => {
    Employee.create(req.body,(err,employee) => {
